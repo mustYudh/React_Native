04b7043acf30be6ff4cbf0cde705b07a383112fa
yudenghao <denghao.yu@yuntu-inc.com>
2019-03-31 16:21:26 +0800

    add：修改文件

--- ReactNativeDemoProject/index.android.js
+++ ReactNativeDemoProject/index.android.js
@@ -23,7 +23,7 @@ export default class ReactNativeDemoProject extends Component {
           To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
+        {'\n'}
           Shake or press menu button for dev menu
         </Text>
       </View>
